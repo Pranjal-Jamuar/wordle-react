465a4b475fac00d70091c4d7bdf4607655a9e9c4
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-03-20 13:12:44 +0530

    Keyboard component started

---
+++ src/components/Key.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import { AppContext } from "../App"
+
+const Key = ({ keyVal, bigKey }) => {
+  const { board, setBoard, currAttempt, setCurrAttempt } =
+    useContext(AppContext)
+
+  const selectLetter = () => {
+    const currBoard = [...board]
+    currBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal
+    setBoard(currBoard)
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 })
+  }
+  return (
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+      {keyVal}
+    </div>
+  )
+}
+
+export default Key
--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import App, { AppContext } from "../App"
+import { AppContext } from "../App"
 
 const Letter = ({ letterPos, attemptVal }) => {
   const { board } = useContext(AppContext)
